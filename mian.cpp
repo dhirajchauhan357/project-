e54d8542c70460a31f8c41c0ef025cc694932f6b
dhirajchauhan357 <dhirajchauhan357@gmail.com>
2024-11-18 09:50:20 -0800

    Add files via upload

---
+++ mian.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+
+{
+    int a,b,c;
+    cout<<"enter a number ";
+    cin>>a;
+    cout<<"enter a second number ";
+    cin>>b;
+    c=a+b;
+    cout<<c;
+    return 0;
+
+
+}
